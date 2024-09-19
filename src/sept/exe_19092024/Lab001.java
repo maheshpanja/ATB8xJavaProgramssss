19505af05f4c4030234f60c2d76e69f311cdf848
maheshpanja <maheshpanja12345@gmail.com>
2024-09-19 20:30:14 +0530

    New file2

--- src/sept/exe_19092024/Lab001.java
+++ src/sept/exe_19092024/Lab001.java
@@ -4,6 +4,8 @@ public class Lab001 {
     public static void main(String[] args)
     {
         System.out.println("Hello World");
+        System.out.println(2*2);
+        System.out.println(222/33444);
     }
 }
 
